e15c8fe5ba833426416a56381aa3cbe8ecdac546
Vijeth Kalavarkar <vkalavarkar321@gmail.com>
2024-12-23 22:01:56 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p className='text-danger text-center'>Lets start our journey here ...</p>
+    </>
+  );
+}
+
+export default App;
